6ae360b6c8336f0cf49f23075af7a51785ed50ad
Abdur Rahman Olamilekan <sulaymanakanji001@gmail.com>
2026-03-30 19:50:22 -0700

    Final project restructuring and alignment for Vercel deployment

--- frontend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
-    HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-    SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
